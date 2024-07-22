dac2f54f3202ead94ce9a365fc5413e3450c8904
AcksBerg <5879689+AcksBerg@users.noreply.github.com>
2024-07-22 18:50:52 +0200

    Update ammo_reader.py

--- ammo_reader.py
+++ ammo_reader.py
@@ -25,9 +25,7 @@ def extract_data(file_path):
         if line.startswith("////"):
             if current_caliber:
                 if current_type:
-                    if "ProjectileCount" not in current_type:
-                        current_type["ProjectileCount"] = "1"
-                    if int(current_type["ProjectileCount"]) > 1:
+                    if "ProjectileCount" in current_type and int(current_type["ProjectileCount"]) > 1:
                         shot_types.append(current_type)
                         shot_sum_type = current_type.copy()
                         shot_sum_type["Damage"] = str(int(current_type["Damage"]) * int(current_type["ProjectileCount"]))
@@ -57,9 +55,7 @@ def extract_data(file_path):
 
         elif line.startswith("//"):
             if current_type:
-                if "ProjectileCount" not in current_type:
-                    current_type["ProjectileCount"] = "1"
-                if int(current_type["ProjectileCount"]) > 2:
+                if "ProjectileCount" in current_type and int(current_type["ProjectileCount"]) > 1:
                     shot_types.append(current_type)
                     shot_sum_type = current_type.copy()
                     shot_sum_type["Damage"] = str(int(current_type["Damage"]) * int(current_type["ProjectileCount"]))
@@ -73,9 +69,7 @@ def extract_data(file_path):
             current_type[key.strip()] = value.strip().strip(";")
 
     if current_type:
-        if "ProjectileCount" not in current_type:
-            current_type["ProjectileCount"] = "1"
-        if int(current_type["ProjectileCount"]) > 2:
+        if "ProjectileCount" in current_type and int(current_type["ProjectileCount"]) > 1:
             shot_types.append(current_type)
             shot_sum_type = current_type.copy()
             shot_sum_type["Damage"] = str(int(current_type["Damage"]) * int(current_type["ProjectileCount"]))
@@ -92,7 +86,20 @@ def extract_data(file_path):
 
     return data_list
 
+def set_default_values(data):
+    keys = ["ProjectileCount", "Damage", "PenetrationPower", "InitialSpeed", "RicochetChance", "FragmentationChance", 
+            "BulletMassGram", "HeavyBleedingDelta", "LightBleedingDelta", "ammoAccr", "ammoHear", "ammoRec", 
+            "malf_changes", "MalfMisfireChance", "MisfireChance", "MalfFeedChance", "DurabilityBurnModificator", 
+            "HeatFactor"]
+
+    for caliber in data:
+        for ammo_type in caliber["types"]:
+            for key in keys:
+                if key not in ammo_type:
+                    ammo_type[key] = 0
+
 data = extract_data(input_file)
+set_default_values(data)
 
 with open(output_file, "w") as json_file:
     json.dump(data, json_file, indent=4)
