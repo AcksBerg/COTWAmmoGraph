8eafbdc6960c87eb05714fea37931e30eb1efb66
AcksBerg <AcksBerg@users.noreply.github.com>
2024-08-13 12:33:55 +0200

    added more data

--- get_live_data.py
+++ get_live_data.py
@@ -16,6 +16,7 @@ query Ammo_query {
   items(lang: en, type: ammo) {
     id
     name
+    weight
     properties {
       ... on ItemPropertiesAmmo {
         penetrationPower
@@ -32,6 +33,7 @@ query Ammo_query {
         failureToFeedChance
         misfireChance
         heatFactor
+        bulletMassGrams
       }
     }
   }
