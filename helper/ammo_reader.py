8eb296319b7ddce15816153bc4af8a4acd3a1168
AcksBerg <AcksBerg@users.noreply.github.com>
2024-09-24 09:55:19 +0200

    Split the Slugs from the shots

--- helper/ammo_reader.py
+++ helper/ammo_reader.py
@@ -17,12 +17,26 @@ try:
     with open(input_file, "r", encoding="UTF-8") as file:
         processed_data = json.load(file)
 except OSError:
-    print("Data file could not be read.")
+    print("input_file could not be read: ", input_file)
     exit()
+
+
+# Copy of processed_data and caliber or it will promped an error for changing size while iteration
+for caliber in list(processed_data):
+    for ammo in processed_data[caliber][:]:
+        # Greater two to keep the Dual Sabot slug as a slug as wished by Fontaine
+        if ammo["ProjectileCount"] > 2:
+            if f'{caliber} shot' not in processed_data:
+                processed_data[f'{caliber} shot'] = [ammo]
+            else:
+                processed_data[f'{caliber} shot'].append(ammo)
+            processed_data[caliber].remove(ammo)
+
+
+            
 
 try:
     with open(ammo_file, "r", encoding="UTF-8") as file:
-        # TODO Add way to split slug and shot
         current_id = None
         current_ammo_pos = None
         for line in file:
@@ -42,12 +56,14 @@ try:
                     processed_data[current_ammo_pos[0]][current_ammo_pos[1]][name] = value
 
 except OSError:
-    print("Ammo.js could not be read")
+    print("Ammo.js could not be read: ", ammo_file)
     exit()
 
-
-with open(data_file, "w", encoding="UTF-8") as file:
-    file.write("const data = ")
-    # Sort the calibers by name. Makes the webside easier to use.
-    json.dump({caliber: processed_data[caliber] for caliber in sorted(processed_data.keys())}, file, indent=4)
-    file.write(";")
+try:
+    with open(data_file, "w", encoding="UTF-8") as file:
+        file.write("const data = ")
+        # Sort the calibers by name. Makes the webside easier to use.
+        json.dump({caliber: processed_data[caliber] for caliber in sorted(processed_data.keys())}, file, indent=4)
+        file.write(";")
+except OSError:
+    print("Could not write the data.js: ", data_file)
